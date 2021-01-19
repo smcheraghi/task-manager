174c20d282a479c876d47d4fe489d9ee30fc1ce4
seyed mohammad cheraghi <35502397+smcheraghi@users.noreply.github.com>
2021-01-18 23:01:10 -0500

    init mongodb connection

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+// CRUD create, read, update, delete
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, {useUnifiedTopology: true}, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to the database!')
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Mohammad',
+        age: 37
+    })
+})
